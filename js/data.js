076f358814f8bf53f29de75db89abf0e9c740535
Gustavo Souza <gustavo.de.souza@outlook.com>
2021-06-22 18:50:05 -0300

    fixed data url

--- js/data.js
+++ js/data.js
@@ -1,73 +1,73 @@
 export const books = [{
     "id": "1",
     "month": "Livro de Janeiro",
-    "author": "url(/images/lucy-foley.jpg)",
-    "url": "url(/images/a-ultima-festa.jpg)"
+    "author": "url(./images/lucy-foley.jpg)",
+    "url": "url(./images/a-ultima-festa.jpg)"
 },
 {
     "id": "2",
     "month": "Livro de Fevereiro",
-    "author": "url(/images/sally-rooney.jpg)",
-    "url": "url(/images/pessoas-normais.jpg)"
+    "author": "url(./images/sally-rooney.jpg)",
+    "url": "url(./images/pessoas-normais.jpg)"
 },
 {
     "id": "3",
     "month": "Livro de Março",
-    "author": "url(/images/bel-rodrigues.jpg)",
-    "url": "url(/images/13-segundos.jpg)"
+    "author": "url(./images/bel-rodrigues.jpg)",
+    "url": "url(./images/13-segundos.jpg)"
 },
 {
     "id": "4",
     "month": "Livro de Abril",
-    "author": "url(/images/stephen-king.jpg)",
-    "url": "url(/images/it-a-coisa.jpg)"
+    "author": "url(./images/stephen-king.jpg)",
+    "url": "url./images/it-a-coisa.jpg)"
 },
 {
     "id": "5",
     "month": "Livro de Maio",
-    "author": "url(/images/john-green.jpg)",
-    "url": "url(/images/quem-e-voce-alaska.jpg)"
+    "author": "url(./images/john-green.jpg)",
+    "url": "url(./images/quem-e-voce-alaska.jpg)"
 },
 {
     "id": "6",
     "month": "Livro de Junho",
-    "author": "url(/images/agatha-christie.jpg)",
-    "url": "url(/images/morte-no-nilo.jpg)"
+    "author": "url(./images/agatha-christie.jpg)",
+    "url": "url(./images/morte-no-nilo.jpg)"
 },
 {
     "id": "7",
     "month": "Livro de Julho",
-    "author": "url(/images/fernanda-torres.jpg)",
-    "url": "url(/images/fim.jpg)"
+    "author": "url(./images/fernanda-torres.jpg)",
+    "url": "url(./images/fim.jpg)"
 },
 {
     "id": "8",
     "month": "Livro de Agosto",
-    "author": "url(/images/donna-dartt.jpg)",
-    "url": "url(/images/o-pintassilgo.jpg)"
+    "author": "url(./images/donna-dartt.jpg)",
+    "url": "url(./images/o-pintassilgo.jpg)"
 },
 {
     "id": "9",
     "month": "Livro de Setembro",
-    "author": "url(/images/raphael-montes.jpg)",
-    "url": "url(/images/uma-mulher-no-escuro.jpg)"
+    "author": "url(./images/raphael-montes.jpg)",
+    "url": "url(./images/uma-mulher-no-escuro.jpg)"
 },
 {
     "id": "10",
     "month": "Livro de Outubro",
-    "author": "url(/images/Taylor-Jenkins-Reide.jpeg)",
-    "url": "url(/images/os-sete-maridos.jpg)"
+    "author": "url(./images/Taylor-Jenkins-Reide.jpeg)",
+    "url": "url(./images/os-sete-maridos.jpg)"
 },
 {
     "id": "11",
     "month": "Livro de Novembro",
-    "author": "url(/images/Suzanne-Collins.jpg)",
-    "url": "url(/images/a-cantiga.jpg)"
+    "author": "url(./images/Suzanne-Collins.jpg)",
+    "url": "url(./images/a-cantiga.jpg)"
 },
 {
     "id": "12",
     "month": "Livro de Dezembro",
-    "author": "url(/images/Jennifer-niven.jpg)",
-    "url": "url(/images/por-lugares-incriveis.jpg)"
+    "author": "url(./images/Jennifer-niven.jpg)",
+    "url": "url(./images/por-lugares-incriveis.jpg)"
 },
 ]
